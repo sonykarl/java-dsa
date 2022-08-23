58608354010eebedf6323c2d52b1546d2b863d0d
sonykarl <karlmaxwatembo@gmail.com>
2022-08-23 14:02:55 +0300

    changes to 2d array

--- src/DataStructures/arrays/Two2DArray.java
+++ src/DataStructures/arrays/Two2DArray.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Two2DArray {
     public static void main(String[] args) {
         int[][] my2darray = new int[][]{{1,2},{3,4}};
-        Arrays.stream(my2darray).toString();
+        var mystring  = Arrays.stream(my2darray).toString();
+        System.out.println(mystring);
     }
 }
