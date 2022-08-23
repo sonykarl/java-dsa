eccd1e31038d5f59ac2b3ad7563d0ad87785827c
sonykarl <karlmaxwatembo@gmail.com>
2022-08-23 13:05:52 +0300

    binary search

---
+++ src/binarySearch/BinarySearch.java
@@ -0,0 +1,46 @@
+package binarySearch;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    void searchBinary(int[] mylist, int target){
+
+        /** sort item in an array first */
+        Arrays.sort(mylist);
+        int startpoint = 0;
+        int endpoint = mylist.length - 1;
+        int midpoint = (startpoint + endpoint) / 2;
+        while (startpoint <= endpoint){
+            if (
+                    mylist[midpoint] < target
+            ) {
+                startpoint = midpoint + 1;
+            } else if (
+                    mylist[midpoint] == target
+            ) {
+                String position = Integer.toString(midpoint);
+                String s = String.format("the target was found at %s",position);
+                System.out.println(s);
+                break;
+            } else {
+                endpoint = midpoint - 1;
+            }
+            midpoint = (startpoint + endpoint) / 2;
+            if (midpoint == endpoint && mylist[midpoint] != target) {
+                System.out.println("target not found");
+                break;
+            }
+
+        }
+
+
+    }
+    public static void main (String[] args) {
+        BinarySearch mySearchInstance = new BinarySearch();
+        int[] mylist = new int[]{0, 4, 5, 23, 1};
+
+        mySearchInstance.searchBinary(mylist,23);
+    }
+}
--- src/linearSearch/LinearSearch.java
+++ src/linearSearch/LinearSearch.java
@@ -2,15 +2,19 @@ package linearSearch;
 
 public class LinearSearch {
 
-    void searchLinearly(String[] mylist){
+    void searchLinearly(String[] mylist, String target){
         int i ;
         for (i = 0; i < mylist.length; i++){
-            System.out.println(mylist[i]);
+            if(mylist[i] == target){
+                String s = String.format("the position is at %s", i);
+                System.out.println(s);
+            }
+
         }
     }
     public static void main(String[] args){
-        String[] mylist = {"cow", "goat","pig","dog","bitch","penguin"};
+        String[] mylist = {"cow", "goat","pig","dog","chicken","penguin"};
         LinearSearch myLinearInstance = new LinearSearch();
-        myLinearInstance.searchLinearly(mylist);
+        myLinearInstance.searchLinearly(mylist, "dog");
     }
 }
