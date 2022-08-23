8874939d96efec2050e0890194b34f05577264ff
sonykarl <karlmaxwatembo@gmail.com>
2022-08-23 14:01:18 +0300

    added 2d array

---
+++ src/DataStructures/arrays/Two2DArray.java
@@ -0,0 +1,10 @@
+package DataStructures.arrays;
+
+import java.util.Arrays;
+
+public class Two2DArray {
+    public static void main(String[] args) {
+        int[][] my2darray = new int[][]{{1,2},{3,4}};
+        Arrays.stream(my2darray).toString();
+    }
+}
