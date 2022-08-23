4be618b077d62598232adcf005c04189bb17db39
sonykarl <karlmaxwatembo@gmail.com>
2022-08-23 13:25:52 +0300

    minor fixes

--- src/Algorithms/linearSearch/LinearSearch.java
+++ src/Algorithms/linearSearch/LinearSearch.java
@@ -4,12 +4,34 @@ public class LinearSearch {
 
     void searchLinearly(String[] mylist, String target){
         int i ;
+        int endpoint = mylist.length -1 ;
         for (i = 0; i < mylist.length; i++){
             if(mylist[i] == target){
-                String s = String.format("the position is at %s", i);
+                String s = String.format("the position is at %s as per method one", i);
                 System.out.println(s);
+                break;
+            }
+            if (i == endpoint && target != mylist[i]){
+                System.out.println("item not found");
+                break;
             }
 
+
+
+        }
+
+        /** method two */
+        for (String listitem: mylist){
+            if (listitem == target){
+                String s = String.format("the position is at %s for method two", i);
+                System.out.println(s);
+                break;
+            }
+
+            if (i == endpoint && target != mylist[i]){
+                System.out.println("item not found");
+                break;
+            }
         }
     }
     public static void main(String[] args){
